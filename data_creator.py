1e58b03828a82014e9a55a68447c59e1b66cba2c
Jorge Ballester <jorge.ballcor@gmail.com>
2025-10-29 18:03:04 +0100

    add: new row to csv

--- data_creator.py
+++ data_creator.py
@@ -13,7 +13,7 @@ os.makedirs("data", exist_ok=True)
 #print("✅ CSV saved in 'data/people.csv'")
 
 
-df = pd.read_csv("data/people.csv")
+"""df = pd.read_csv("data/people.csv")
 new_row = {"name": "David", "age": 40}
 df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
 
@@ -21,4 +21,13 @@ df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
 df.to_csv("data/people.csv", index=False)
 
 
+print("✅ Added new row to CSV")
+"""
+
+df = pd.read_csv("data/people.csv")
+new_row = {"name": "Emma", "age": 28}
+df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
+
+df.to_csv("data/people.csv", index=False)
 print("✅ Added new row to CSV")
